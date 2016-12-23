025dd43c89055288bcc800cf21d3149d81638153
Guikingone <guillaume.loulier@hotmail.fr>
2016-12-23 15:33:44 +0100

    [+][Core][!M][Init] || Init vuejs|Done + Init Foundation|Done

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Class HomeController
+ * @package AppBundle\Controller
+ *
+ * @author Guillaume Loulier <contact@guillaumeloulier.fr>
+ */
+class HomeController extends Controller
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction()
+    {
+        return $this->render('Home/index.html.twig');
+    }
+}
